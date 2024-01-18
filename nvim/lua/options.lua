b50212c4a751f95a37198ab1ae9e05e3321b7b8d
Eric Gericke <70900221+Eric-the-Geric@users.noreply.github.com>
2024-01-18 13:17:00 +0100

    Add files via upload

---
+++ nvim/lua/options.lua
@@ -0,0 +1,21 @@
+vim.cmd([[
+:set number
+:set relativenumber
+:set autoindent
+:set tabstop=4
+:set shiftwidth=4
+:set smarttab
+:set softtabstop=4
+:set mouse=a
+:set nohlsearch
+:set hidden
+:set incsearch
+:set scrolloff=8
+:set colorcolumn=80
+:set signcolumn=yes
+:set cmdheight=2
+:set nowrap
+:set tgc
+]])
+
+vim.g.mapleader = " "
