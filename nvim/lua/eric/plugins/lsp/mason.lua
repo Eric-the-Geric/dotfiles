caa11266709de450ac9e2934f15da0e83bd7c51b
Eric-the-Geric <eric.gericke2@gmail.com>
2024-01-26 02:52:40 +0100

    Finally, lsp working -need to install pyright w/ npm

--- nvim/lua/eric/plugins/lsp/mason.lua
+++ nvim/lua/eric/plugins/lsp/mason.lua
@@ -11,7 +11,7 @@ return {
     -- import mason-lspconfig
     local mason_lspconfig = require("mason-lspconfig")
 
-    local mason_tool_installer = require("mason-tool-installer")
+    ---local mason_tool_installer = require("mason-tool-installer")
 
     -- enable mason and configure icons
     mason.setup({
@@ -34,13 +34,13 @@ return {
       automatic_installation = true, -- not the same as ensure_installed
     })
 
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint", -- python linter
-      },
-    })
+    --mason_tool_installer.setup({
+      --ensure_installed = {
+        --"stylua", -- lua formatter
+        --"isort", -- python formatter
+        --"black", -- python formatter
+        --"pylint", -- python linter
+      --},
+    --})
   end,
 }
