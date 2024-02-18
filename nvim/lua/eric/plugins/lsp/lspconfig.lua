66778f775c336ee727c1966182c50d88f3a3b1ba
Eric-the-Geric <eric.gericke2@gmail.com>
2024-02-18 01:04:59 +0100

    added c to neovim

--- nvim/lua/eric/plugins/lsp/lspconfig.lua
+++ nvim/lua/eric/plugins/lsp/lspconfig.lua
@@ -99,5 +99,20 @@ return {
         },
       },
     })
+	lspconfig['clangd'].setup({
+		capabilities = capabilities,
+		on_attach = on_attach,
+		filetypes = { "h", "c", "cpp", "cc", "objc", "objcpp"},
+		cmd = {"clangd", "--background-index"},
+		single_file_support = true,
+		root_dir = lspconfig.util.root_pattern(
+			  '.clangd',
+			  '.clang-tidy',
+			  '.clang-format',
+			  'compile_commands.json',
+			  'compile_flags.txt',
+			  'configure.ac',
+			  '.git'
+		  )})
   end,
 }
